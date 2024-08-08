9b9ae9f5c7d577ca99b38f9edafd9b3d9e4a28fb
Jhon G. Botello <jbote001@odu.edu>
2024-08-08 12:55:19 -0400

    Update mainllama.py

--- mainllama.py
+++ mainllama.py
@@ -1,6 +1,4 @@
-import streamlit as st
-from streamlit_chat import message
-from langchain_community.chat_models import ChatOllama
+from langchain_community.chat_models import ChatOpenAI
 from langchain.chains import ConversationChain
 from langchain.chains.conversation.memory import ConversationBufferWindowMemory
 from langchain.prompts import (
@@ -9,49 +7,38 @@ from langchain.prompts import (
     ChatPromptTemplate,
     MessagesPlaceholder
 )
+import streamlit as st
+from streamlit_chat import message
+from utils import *
 import emoji
-from utils import find_match
-from requests.exceptions import ConnectionError
 
 # Create a header with the title of the app
 st.subheader("CSChat")
 
-# Initialize responses and requests in session state if not already present
+# Two variables (responses and requests) are checked if they are present in the Streamlit session state. If they are not present, they are initialized with default values.
 if 'responses' not in st.session_state:
-    st.session_state['responses'] = ["Hi There! I am John AI. I was designed to Help Dr. Weigle to answer your questions about anything related to the CS Handbook. How can I assist you?"]
+    st.session_state['responses'] = ["Hi There! I am John AI. i was designed to Help Dr. Weigle to answer your questiosn about anything related to the CS Handbook. How can I assist you?"]
 
 if 'requests' not in st.session_state:
     st.session_state['requests'] = []
 
-# Initialize conversational model
-try:
-    llm = ChatOllama(
-        model_name="mistral:7b-instruct-v0.2-fp16",
-        api_key=st.secrets["OLLAMA_API_KEY"],
-        server_url="http://localhost:11434"
-    )
 
-    # Initialize conversation memory
-    if 'buffer_memory' not in st.session_state:
-        st.session_state.buffer_memory = ConversationBufferWindowMemory(k=3, return_messages=True)
+# initialize conversational model
+llm = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key="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")
 
-    # Message templates
-    system_msg_template = SystemMessagePromptTemplate.from_template(template="Answer the question as truthfully as possible using your knowledge and the provided context")
-    human_msg_template = HumanMessagePromptTemplate.from_template(template="{input}")
-    prompt_template = ChatPromptTemplate.from_messages([system_msg_template, MessagesPlaceholder(variable_name="history"), human_msg_template])
+# Conversation memory configuration
+if 'buffer_memory' not in st.session_state:
+            st.session_state.buffer_memory=ConversationBufferWindowMemory(k=3,return_messages=True)
 
-    # Initialize conversation chain
-    conversation = ConversationChain(
-        memory=st.session_state.buffer_memory,
-        prompt=prompt_template,
-        llm=llm,
-        verbose=True
-    )
+# Message templates
+system_msg_template = SystemMessagePromptTemplate.from_template(template="Answer the question as truthfully as possible using your knoledge and the provided context")
 
-except ConnectionError:
-    st.error("Failed to connect to the ChatOllama server. Please check the server status and try again.")
+human_msg_template = HumanMessagePromptTemplate.from_template(template="{input}")
 
-# Function to display messages
+prompt_template = ChatPromptTemplate.from_messages([system_msg_template, MessagesPlaceholder(variable_name="history"), human_msg_template])
+
+conversation = ConversationChain(memory=st.session_state.buffer_memory, prompt=prompt_template, llm=llm, verbose=True)
+
 def display_message(text, is_user=False):
     if is_user:
         st.markdown(f"""
@@ -72,26 +59,30 @@ def display_message(text, is_user=False):
         </div>
         """, unsafe_allow_html=True)
 
-# Container for chat history
+
+# container for chat history
 response_container = st.container()
+# container for text box
+textcontainer = st.container()
 
-# Container for text input
-text_container = st.container()
+with textcontainer:
 
-# Input text and generate response
-with text_container:
     query = st.text_input("Query: ", key="input")
-    if query:
-        if 'conversation' in globals():
-            with st.spinner("typing..."):
-                context = find_match(query)
-                response = conversation.predict(input=f"Context:\n {context} \n\n Query:\n{query}")
-            st.session_state.requests.append(query)
-            st.session_state.responses.append(response)
-        else:
-            st.warning("ChatOllama server connection is not established. Please try again later.")
 
-# Display chat history
+    if query:
+        with st.spinner("typing..."):
+            #conversation_string = get_conversation_string()
+            ## st.code(conversation_string)
+            #refined_query = query_refiner(conversation_string, query)
+            #st.subheader("Refined Query:")
+            #st.write(refined_query)
+            context = find_match(query)
+            ## print(context)  
+            response = conversation.predict(input=f"Context:\n {context} \n\n Query:\n{query}")
+        st.session_state.requests.append(query)
+        st.session_state.responses.append(response)
+
+
 with response_container:
     if st.session_state['responses']:
         for i in range(len(st.session_state['responses'])):
